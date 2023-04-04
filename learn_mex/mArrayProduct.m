b807cd89d7b6598c23f18383b60f57042fceb4a0
kattmabm <kattmabm@rose-hulman.edu>
2023-04-04 10:36:38 -0400

    Added documentation to file

--- learn_mex/mArrayProduct.m
+++ learn_mex/mArrayProduct.m
@@ -1,4 +1,14 @@
-function [output] = mArrayProduct(multiplier, array)
+function [product] = mArrayProduct(multiplier, array)
+% MATLAB interface for the main C function found in arrayProduct.c
+%
+% INPUTS
+% multiplier -- a scalar value
+% array      -- a row vector
+%
+% OUTPUTS
+% product    -- new row vector containing all elements of array multiplied
+%               by multiplier
+%
 mex arrayProduct.c
-output = arrayProduct(multiplier, array);
+product = arrayProduct(multiplier, array);
 end
