af76918a3c0800bbf1c0dd2afe4923edaa9abbb0
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-04-18 15:30:44 -0400

    Created four files which modify the Exercise1_PointTypes to achieve a 1-Norm, 2-Norm, 3-Norm, and Inf-Norm boundary.

---
+++ Inf_Norm_PointTypes.m
@@ -0,0 +1,70 @@
+function [pt3] = Inf_Norm_PointTypes(density, r, plot)
+% Only solves top right quadrant, then pass through a function to reflect
+% to the other four quadrants.
+%
+% INPUTS
+% density -- density of points to generate
+%       r -- width of x and y range
+%    plot -- whether or not to display results (DEFUALT: 0)
+%
+% OUTPUTS
+% pointTypes -- a matrix containing the point types of each point according
+% to the encoding below
+%
+% POINT TYPES
+% Type 0: Outside the curve
+% Type 1: UNUSED!!!
+% Type 2: On circular boundary
+% Type 3: Interior
+
+% Set plot to 0 if no argument was provided
+if ~exist("plot", "var") || isempty(plot)
+    plot = 0;
+end
+
+% Define a function that converts from point to index
+ptoi = @(x) round(1 + (x/density));
+
+% Initialize point types 2D matrix
+xLen = (r/density)+2;
+yLen = (r/density)+2;
+pointTypes = zeros(xLen, yLen);
+
+% Defines how wide to make the border on the curve
+% Default value of 10. To make thicker line, make widthFactor smaller
+widthFactor = 10;
+
+% Define all points on and inside curve
+maxDist = density/widthFactor/sqrt(2);
+for x = 0 : density : r
+    for y = 0 : density : r
+        dist = max(x,y) - r;
+        if dist < -maxDist
+            % Interior
+            pointTypes(ptoi(x), ptoi(y)) = 3;
+        elseif dist <= maxDist
+            % On Circle Boundary
+            pointTypes(ptoi(x), ptoi(y)) = 2;
+        end
+    end
+end
+
+% % Define all points on straight line
+% x = r;
+% for y = 0 : density : r
+%     pointTypes(ptoi(x), ptoi(y)) = 1;
+% end
+
+pointTypes = pointTypes';
+
+% Reflect the points across themselves to complete the shape
+pt2 = cat(1, flip(pointTypes, 1), pointTypes);
+pt3 = cat(2, flip(pt2, 2), pt2);
+
+% If plot is set to true, plot the points by color
+if plot ~= 0
+    figure(plot);
+    clf;
+    axis equal;
+    pcolor(pt3);
+end
---
+++ boundary_testing.m
@@ -0,0 +1,20 @@
+% File used for testing boundary things such as PointTypes
+close;
+clear;
+clc;
+
+pt_types = Exercise1_PointTypes(0.005,0.1,1);
+pt_types2   = Circle_PointTypes(0.005,0.1,2);
+pt_types3   = One_Norm_PointTypes(0.005,0.1,3);
+pt_types4   = Three_Norm_PointTypes(0.005,0.1,4);
+pt_types5   = Inf_Norm_PointTypes(0.005,0.1,5);
+
+x_vec   = -1:0.001:1;
+y_vec1  = (1-abs(x_vec.^3)).^(1/3);
+y_vec2  = (abs(x_vec.^3)-1).^(1/3);
+
+% figure(6);
+clf;
+plot(x_vec, y_vec1);
+hold on;
+plot(x_vec, -y_vec1);
