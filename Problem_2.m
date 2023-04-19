4ca9410bbe1a790e6e82635c41e6373de8f9e09d
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-04-18 20:20:26 -0400

    Tried to make a rubber band model. Looks pretty okay-ish

--- Problem_2.m
+++ Problem_2.m
@@ -4,41 +4,110 @@ clear;
 clc;
 
 % Constants
-T_max   = 50;
-L       = 2;
+T_max   = 30;
+L       = 0.1;
 % kappa   = 0.971796754;    % Minimum where no negative displacement
-kappa_g = 0.1;
+kappa_g = 0.5;
 
-T_optim     = 6:0.5:T_max;
-kappa_optim = zeros(size(T_optim));
-for i = 1:length(T_optim)
-    kappa_optim(i) = fmincon(@(k)func_resid_Pb2(k, T_optim(i), L), kappa_g, -1, 0);
+% Rubber Band Constants
+h       = 5;
+x_pull  = L/2;
+fast_forward    = 5;
+
+% optimize?
+optim   = 0;
+movie   = 1;
+test_k  = 0;
+r_band  = 1;
+
+% Find optimal kappa for each value
+if (optim)
+    T_optim     = 6:0.5:T_max;
+    kappa_optim = zeros(size(T_optim));
+    for i = 1:length(T_optim)
+        kappa_optim(i) = fmincon(@(k)func_resid_Pb2(k, T_optim(i), L), kappa_g, -1, 0);
+    end
+
+    % Find u_mat and x_vec
+    [u_mat, x_vec, t_vec, delX, delT]  = get_u_mat(kappa_optim(end), T_optim(end), L);
+    disp("Minimum displacement: " + min(min(u_mat)));
 end
 
-% Find u_mat and x_vec
-[u_mat, x_vec, t_vec, delX, delT]  = get_u_mat(kappa_optim(end), T_optim(end), L);
-
-disp("Minimum displacement: " + min(min(u_mat)));
+% Test multiple kappa values and display them on the same graph
+if (test_k)
+    % Initial Test
+    T   = 15;
+    [u_mat_0,~,~,~,~] = get_u_mat(0,T,L);
+    kappa_test  = linspace(0,1,6);
+    % Setup for Loop
+    u_max_solns = zeros(length(kappa_test), width(u_mat_0));
+    u_min_solns = zeros(length(kappa_test), width(u_mat_0));
+    % Finding min/max vectors for each test
+    for i = 1:length(kappa_test)
+        [u_mat,x_vec_plt,~,~,~] = get_u_mat(kappa_test(i), T, L);
+        [u_max_solns(i,:), u_min_solns(i,:)]    = find_u_max_min(u_mat);
+    end
+end
 
 % Kappa Plot
-figure(1);
-clf;
-plot(T_optim, kappa_optim);
-xlabel("Time (s)");
-ylabel("Optimal Kappa Value");
+if (optim)
+    figure(1);
+    clf;
+    plot(T_optim, kappa_optim);
+    xlabel("Time (s)");
+    ylabel("Optimal Kappa Value");
+end
+
+% Rubber Band Testing
+if (r_band)
+    [u_mat, x_vec, t_vec, delX, delT]   = r_band_u_mat(kappa_g, T_max, L, h, x_pull);
+end
+
+% Testing kappa plot
+if (test_k)
+    % Max Plot
+    figure(2);
+    clf;
+    legend_labels   = strings(size(kappa_test));
+    for i = 1:length(kappa_test)
+        plot(x_vec_plt, u_max_solns(i,:));
+        hold on;
+        legend_labels(i)    = "kappa = " + kappa_test(i);
+    end
+    xlabel("x location");
+    ylabel("u displacement");
+    legend(legend_labels);
+
+    % Min Plot
+    figure(3);
+    clf;
+    for i = 1:length(kappa_test)
+        plot(x_vec_plt, u_min_solns(i,:));
+        hold on;
+    end
+    xlabel("x location");
+    ylabel("u displacement");
+    legend(legend_labels);
+end
 
 % Movie
-figure(2);
-clf;
-for i = 1:length(t_vec)
-    figure(2);
-    plot(x_vec, u_mat(i,:));
-    xlim([0,L]);
-    ylim([-1,1]);
-    xlabel("x position");
-    ylabel("u displacement");
-    pause(delT);
+if (movie)
+    movie_plt   = 4;
+    figure(movie_plt);
+    clf;
+    max_u   = max(max(u_mat));
+    min_u   = min(min(u_mat));
+    for i = 1:length(t_vec)
+        figure(movie_plt);
+        plot(x_vec, u_mat(i,:));
+        xlim([0,L]);
+        ylim([min_u,max_u]);
+        xlabel("x position");
+        ylabel("u displacement");
+        pause(delT/fast_forward);
+    end
 end
+
 
 %% Helpful Functions
 function [f] = func_resid_Pb2(kappa, T, L)
@@ -51,4 +120,11 @@ function [f] = func_resid_Pb2(kappa, T, L)
     if (u_min < 0) 
         f = f - u_min*10000;
     end
+end
+
+function [u_max, u_min] = find_u_max_min(u_mat)
+    [~, maxRowInd] = max(sum(u_mat,2));
+    [~, minRowInd] = min(sum(u_mat,2));
+    u_max   = u_mat(maxRowInd,:);
+    u_min   = u_mat(minRowInd,:);
 end
---
+++ r_band_u_mat.m
@@ -0,0 +1,44 @@
+function [u_mat, x_vec, t_vec, delX, delT] = r_band_u_mat(kappa, T, L, h, x_pull)
+    % Constants
+    H_rho   = 0.1;
+    delX    = 0.01;
+    delT    = 0.028;
+    if (~exist("h", "var")) 
+        h   = L/4;
+    end
+    if (~exist("x_pull", "var")) 
+        x_pull  = L/2;
+    end
+    
+    % Vectors
+    x_vec   = 0:delX:L;
+    t_vec   = 0:delT:T;
+    
+    % Solution
+    u_mat   = zeros(length(t_vec), length(x_vec));
+
+    % Initial Conditions u(t,x)
+    u_mat(1,x_vec<=x_pull)  = (h/x_pull)*x_vec(x_vec<=x_pull);
+    u_mat(1,x_vec>x_pull)   = -h/(L-x_pull)*x_vec(x_vec>x_pull) + h*L/(L-x_pull);
+    u_mat(2,:)  = u_mat(1,:);
+    
+    % Time Stepping
+    x_change    = 2:length(x_vec)-1;
+    for t_pos = 2:length(t_vec)-1
+        % Past u values
+        uix1    = u_mat(t_pos,x_change+1);
+        ui      = u_mat(t_pos,x_change);
+        uix_1   = u_mat(t_pos,x_change-1);
+        uit_1   = u_mat(t_pos-1,x_change);
+    
+        % Coefficients
+        ai      = H_rho/delX^2;
+        bi      = (2/delT^2 - 2*H_rho/delX^2);
+        ci      = ai;
+        di      = (kappa/2/delT - 1/delT^2);
+        deni    = 1/delT^2 + kappa/2/delT;
+    
+        % Next u_mat
+        u_mat(t_pos+1,x_change)     = (ai*uix1 + bi*ui + ci*uix_1 + di*uit_1)/deni;
+    end
+end
