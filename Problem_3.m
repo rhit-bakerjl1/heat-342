ca6a2afdf485d6e0f11333fe44892c0a68a77786
rhit-martina9 <89958644+rhit-martina9@users.noreply.github.com>
2023-04-23 22:00:27 -0400

    Exercise 3 new video

--- Problem_3.m
+++ Problem_3.m
@@ -7,21 +7,23 @@ p=2;
 Lx=2;
 Ly=2;
 alpha = 2;
-dx = 0.05;
-dy = 0.05;
-dt = .95*dx*dx*sqrt(1/alpha);
-T = 3000*dt;
+dx = 0.01;
+dy = 0.01;
+%dt = .99*dx*dy*sqrt(1/2);
+dt = .0028;
+T = 20;
 PosX = unique([0-Lx/2:dx:Lx/2,Lx/2]);
 PosY = unique([0-Ly/2:dy:Ly/2,Ly/2]);
 Time = unique([0:dt:T,T]);
 Sol = zeros(length(PosX), length(PosY), length(Time));
+R = zeros(length(PosX), length(PosY));
 for i=1:length(PosX)
     for j=1:length(PosY)
-        v = [PosX(i) PosY(j)];
-        if abs(norm(v,p)-1) <= min(dx,dy)/1.5
+        R(i,j)=norm([PosX(i) PosY(j)],p);
+        if abs(R(i,j)-1) <= min(dx,dy)/1.5
             Sol(i,j,1) = 0.1;
         end
-        if norm(v,p) <= 1+min(dx,dy)/1.5
+        if R(i,j) <= 1+min(dx,dy)/1.5
             Sol(i,j,1) = (PosX(i)^2+PosY(j)^2)/10;
         end
         Sol(i,j,2) = Sol(i,j,1);
@@ -35,14 +37,12 @@ for k = 3:length(Time)
     for i = 1:length(PosX)
         for j = 1:length(PosY)
 %             Sol(i,j,k)=Sol(i,j,k-1);
-            v = [PosX(i) PosY(j)];
-            r = norm(v,p);
-            if r < 1-min(dx,dy)/1.5
+            if R(i,j) < 1-min(dx,dy)/1.5
                 Sol(i,j,k) = 2*Sol(i,j,k-1) - Sol(i,j,k-2) + ...
                     rx*(Sol(i+1,j,k-1) - 2*Sol(i,j,k-1) + ...
                     Sol(i-1,j,k-1)) + ry*(Sol(i,j+1,k-1) - ...
                     2*Sol(i,j,k-1) + Sol(i,j-1,k-1));
-            elseif abs(r-1) <= min(dx,dy)/1.5
+            elseif abs(R(i,j)-1) <= min(dx,dy)/1.5
                 Sol(i,j,k) = .1;
             else
                 Sol(i,j,k) = 0;
